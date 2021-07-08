1b1f34b45a62b5a36eb989eba65b3e0e7a2efb84
letsbuild-with-rajesh <86154498+letsbuild-with-rajesh@users.noreply.github.com>
2021-07-08 21:57:44 +0530

    commit-11
    Added strike animation when won.
    
    Signed-off-by: letsbuild-with-rajesh <86154498+letsbuild-with-rajesh@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -6,56 +6,51 @@ function App() {
   const ANIMATION_DELAY = 100;
   const MAX_MOVES = MATRIX_SIZE * MATRIX_SIZE;
   const EMPTY_CELL = '&nbsp;&nbsp;&nbsp;';
-  const ID_PREFIX = 'cell';
+  const CELL_ID_PREFIX = 'cell';
+  const ANIMATE_STEPS = 10;
+  const STRIKE_ANIMATION_SECONDS = 0.5;
 
   var gameState;
   var movesMade = 0;
   var hasAnyOneWon = false;
   var isUserAllowedToPlay = true;
+  var wonIds = [];
 
+  const drawStrikeLine = (startId, endId, animate) => {
+    let strikeLineEl = document.getElementById("strikeLine");
+    let startEl = document.getElementById(startId);
+    let endEl = document.getElementById(endId);
 
-  const draw = (posArr1, posArr2) => {
-    const canvas = document.getElementsByTagName('canvas')[0];
+    let x1 = startEl.getBoundingClientRect().left + (startEl.offsetWidth / 2);
+    let y1 = startEl.getBoundingClientRect().top + (startEl.offsetHeight / 2);
+    let x2 = endEl.getBoundingClientRect().left + (endEl.offsetWidth / 2);
+    let y2 = endEl.getBoundingClientRect().top + (endEl.offsetHeight / 2);
 
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+    if (animate) {
+      let xperstep = (x2 - x1) / ANIMATE_STEPS;
+      let yperstep = (y2 - y1) / ANIMATE_STEPS;
 
-    if (!canvas.getContext) {
-        return;
+      const updateCoordinates = (iter = 1) => {
+        if (iter === 1) {
+          strikeLineEl.setAttribute('x1', x1);
+          strikeLineEl.setAttribute('y1', y1);
+        }
+
+        strikeLineEl.setAttribute('x2', x1 + (xperstep * iter));
+        strikeLineEl.setAttribute('y2', y1 + (yperstep * iter));
+
+        if (iter <= ANIMATE_STEPS) {
+          setTimeout(() => { updateCoordinates(iter + 1); }, (STRIKE_ANIMATION_SECONDS * 1000) / ANIMATE_STEPS);
+        }
+      }
+
+      updateCoordinates();
+    } else {
+      strikeLineEl.setAttribute('x1', x1);
+      strikeLineEl.setAttribute('y1', y1);
+      strikeLineEl.setAttribute('x2', x2);
+      strikeLineEl.setAttribute('y2', y2);
     }
-    const ctx = canvas.getContext('2d');
-
-    let lx = 0, ly = 0;
-    let rx = 0, ry = 0;
-    lx = getElementTopLeft(getIdFromIndices(posArr1[0], posArr1[1])).left + canvas.offsetWidth/10;
-    ly = getElementTopLeft(getIdFromIndices(posArr1[0], posArr1[1])).top - canvas.offsetHeight/10;
-
-    rx = getElementTopLeft(getIdFromIndices(posArr2[0], posArr2[1])).left + canvas.offsetWidth/10;
-    ry = getElementTopLeft(getIdFromIndices(posArr2[0], posArr2[1])).top + canvas.offsetWidth/10;
-
-    // set line stroke and line width
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 4;
-
-    // draw a red line
-    ctx.beginPath();
-    ctx.moveTo(lx, ly);
-    ctx.lineTo(rx, ry);
-    ctx.stroke();
-  }
-
-  const getElementTopLeft = (id) =>{
-    var ele = document.getElementById(id);
-    var top = 0;
-    var left = 0;
-
-    while(ele.tagName !== "BODY") {
-        top += ele.offsetTop;
-        left += ele.offsetLeft;
-        ele = ele.offsetParent;
-    }
-
-    return { top: top, left: left };
   }
 
   const resetGame = () => {
@@ -65,11 +60,16 @@ function App() {
       for (let j = 0; j < MATRIX_SIZE; j++) {
         gameState[i][j] = '';
         document.getElementById(getIdFromIndices(i, j)).innerHTML = EMPTY_CELL;
-        document.getElementById("message").innerHTML = '';
+        document.getElementById("message").innerHTML = '&nbsp;';
       }
     }
     movesMade = 0;
     hasAnyOneWon = false;
+    let strikeLineEl = document.getElementById("strikeLine");
+    strikeLineEl.setAttribute('x1', 0);
+    strikeLineEl.setAttribute('y1', 0);
+    strikeLineEl.setAttribute('x2', 0);
+    strikeLineEl.setAttribute('y2', 0);
   }
 
   const updateGameState = (i, j, val) => {
@@ -90,7 +90,7 @@ function App() {
   }
 
   const getIdFromIndices = (i, j) => {
-    return ID_PREFIX + (i + 1) + (j + 1);
+    return CELL_ID_PREFIX + (i + 1) + (j + 1);
   }
 
   const canContinueGame = () => {
@@ -147,14 +147,11 @@ function App() {
         const pos3 = lv;
 
         if (pos1 === pos3 && pos2 === pos3) {
-          /*draw([
-            Math.min(calcforx, calcsorx, li),
-            Math.min(calcfory, calcsory, lj)
-          ],[
-            Math.max(calcforx, calcsorx, li),
-            Math.max(calcfory, calcsory, lj)
-          ]
-          );*/
+          wonIds = [];
+          wonIds.push(getIdFromIndices(Math.min(calcforx, calcsorx, li), Math.min(calcfory, calcsory, lj)));
+          wonIds.push(getIdFromIndices(Math.max(calcforx, calcsorx, li), Math.max(calcfory, calcsory, lj)));
+
+          drawStrikeLine(wonIds[0], wonIds[1], true);
           return true;
         }
       }
@@ -184,10 +181,10 @@ function App() {
     */
 
     hasWon = isItAStrike(
-      [ -1, -1, 0, 1, 1, 1, 0, -1 ],
-      [ 0, 1, 1, 1, 0, -1, -1, -1 ],
-      [ -2, -2, 0, 2, 2, 2, 0, -2 ],
-      [ 0, 2, 2, 2, 0, -2, -2, -2 ],
+      [-1, -1, 0, 1, 1, 1, 0, -1],
+      [0, 1, 1, 1, 0, -1, -1, -1],
+      [-2, -2, 0, 2, 2, 2, 0, -2],
+      [0, 2, 2, 2, 0, -2, -2, -2],
       lasti,
       lastj,
       lastVal
@@ -206,31 +203,38 @@ function App() {
 
     // Skip if already won with previous check
     hasWon = hasWon || isItAStrike(
-      [ -1, -1, 0, 1 ],
-      [ 0, 1, 1, 1 ],
-      [ 1, 1, 0, -1 ],
-      [ 0, -1, -1, -1 ],
+      [-1, -1, 0, 1],
+      [0, 1, 1, 1],
+      [1, 1, 0, -1],
+      [0, -1, -1, -1],
       lasti,
       lastj,
       lastVal
     );
 
-    if (hasWon){
+    if (hasWon) {
       hasAnyOneWon = hasWon;
-      document.getElementById("message").innerHTML = (player === "User" ? "You": player) + ' won the game!';
+      document.getElementById("message").innerHTML = (player === "User" ? "You" : player) + ' won the game!';
     } else if (movesMade >= MAX_MOVES) {
-      document.getElementById("message").innerHTML ='It\'s a tie!';
+      document.getElementById("message").innerHTML = 'It\'s a tie!';
     }
     return hasWon;
   }
 
   window.onload = resetGame;
+  window.onresize = () => {
+    if (hasAnyOneWon) {
+      drawStrikeLine(wonIds[0], wonIds[1], false);
+    }
+  };
 
   return (
     <div className="App">
       <h2>Tic Tac Toe!</h2>
       <h3>Shall we play? &#128512;</h3>
-      <canvas />
+      <svg>
+        <line id="strikeLine"></line>
+      </svg>
       <table>
         <tbody>
           {Array.from(Array(MATRIX_SIZE).keys()).map((rowVal, i) => {
@@ -238,16 +242,16 @@ function App() {
               <tr key={"row" + i}>
                 {Array.from(Array(MATRIX_SIZE).keys()).map((cellVal, j) => {
                   let id = getIdFromIndices(i, j);
-                  return <td key={"key-" + id} onClick={() => { playUsersMove(i, j) }}><p className="cell" id={id}/></td>
+                  return <td key={"key-" + id} onClick={() => { playUsersMove(i, j) }}><p className="cell" id={id} /></td>
                 })}
               </tr>)
           })}
         </tbody>
       </table>
       <br />
-      <p id="message"></p>
+      <p id="message">&nbsp;</p>
       <br />
-      <button onClick={()=>{resetGame()}}>Reset Game</button>
+      <button onClick={() => { resetGame() }}>Reset Game</button>
     </div>
   );
 }
